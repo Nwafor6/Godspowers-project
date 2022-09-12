7bfb2606d13494187749233104b0d98ee58bcf6c
Glory <nwaforglory6@gmail.com>
2022-09-12 22:56:22 +0100

    first commit for Godspowers porject

---
+++ static/JavaScript/index.js
@@ -0,0 +1,26 @@
+
+function opennav() {
+  document.getElementById("navlink").style.width = "200px";
+
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closenav() {
+  document.getElementById("navlink").style.width = "0";
+}
+function closeright() {
+    document.getElementById("linkd").style.width = '-250px';
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
